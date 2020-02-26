ff10f411a35c2a2acf1a5d4e88e83e64875349b9
krgizdov <lumpkinzg@gmail.com>
2020-02-26 22:32:14 +0200

    SimpleArraySum task completed

---
+++ Problem Solving/Algorithms/C#/SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleArraySum
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine();
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sumOfArray = CalculateSimpleArraySum(arr);
+
+            Console.WriteLine(sumOfArray);
+        }
+
+        static int CalculateSimpleArraySum(int[] arr)
+        {
+            int sumOfArray = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sumOfArray += arr[i];
+            }
+
+            return sumOfArray;
+        }
+    }
+}
