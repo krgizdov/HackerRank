3a739a0e7a8e26701fa9053d02338520d93601ff
krgizdov <lumpkinzg@gmail.com>
2020-12-29 03:20:28 +0200

    Solved SolveMeFirst challenge.

---
+++ Problem Solving/Algorithms/C#/SolveMeFirst/SolveMeFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = SolveMeFirst(num1, num2);
+            Console.WriteLine(sum);
+        }
+
+        static int SolveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
